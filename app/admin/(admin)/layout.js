689fd3d5b17c5758bc5e548e543fd138794e1518
Talha Aleem <aleemtalha098@gmail.com>
2025-05-28 03:11:34 +0500

    first commit

--- app/admin/(admin)/layout.js
+++ app/admin/(admin)/layout.js
@@ -3,13 +3,13 @@ import AdminSidebar from "@/components/AdminSidebar";
 
 export default function AdminLayout({ children }) {
   return (
-    <AdminAuthGuard>
+    // <AdminAuthGuard>
       <div className="h-screen flex overflow-hidden">
         <AdminSidebar />
         <main className="flex-1 h-full overflow-y-auto p-8 bg-gray-100">
           {children}
         </main>
       </div>
-    </AdminAuthGuard>
+    // </AdminAuthGuard>
   );
 }
